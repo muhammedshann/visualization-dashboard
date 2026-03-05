80f3a88fc755ac56978ebafe2d3dc09f76ec9820
Muhammed Shan <muhammedshan930o@gmail.com>
2026-03-05 16:44:27 +0530

    added frontend domain into backend cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -161,5 +161,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    "https://visualdashboard-pi.vercel.app",
 ]
