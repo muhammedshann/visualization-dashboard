ca68e62f3b8c03e378d41574704278a035e900b1
Muhammed Shan <muhammedshan930o@gmail.com>
2026-03-09 16:02:14 +0530

    allowed host changed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ["https://visualization-dashboard-pugc.onrender.com"]
+ALLOWED_HOSTS = ['visualization-dashboard-pugc.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
